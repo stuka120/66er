070fd5da120cad63679ab542c43ab013cc9507e2
Dominik Schiener <dominik.schiener@mv-data.at>
2019-11-21 19:55:24 +0100

    * Fixed height problems of card slider in lg view * Removed the wagggs and wosm * Fixed config not loaded problem for the swiper

--- src/app/components/stufen-slide-swiper/stufen-slide-swiper.component.ts
+++ src/app/components/stufen-slide-swiper/stufen-slide-swiper.component.ts
@@ -1,22 +1,25 @@
-import { Component, OnInit, Input, ViewChild } from '@angular/core';
-import { StufenCardModel } from "../../model/stufen-card.model";
+import {Component, OnInit, Input, ViewChild, Inject} from "@angular/core";
+import {StufenCardModel} from "../../model/stufen-card.model";
 import {
+  SWIPER_CONFIG,
+  SwiperConfigInterface,
   SwiperDirective
 } from "ngx-swiper-wrapper";
 
 @Component({
-  selector: 'app-stufen-slide-swiper',
-  templateUrl: './stufen-slide-swiper.component.html',
-  styleUrls: ['./stufen-slide-swiper.component.css']
+  selector: "app-stufen-slide-swiper",
+  templateUrl: "./stufen-slide-swiper.component.html",
+  styleUrls: ["./stufen-slide-swiper.component.css"]
 })
 export class StufenSlideSwiperComponent implements OnInit {
   @Input() stufenModels: StufenCardModel[];
 
-  @ViewChild(SwiperDirective, { static: false }) directiveRef?: SwiperDirective;
+  @ViewChild(SwiperDirective, {static: false}) directiveRef?: SwiperDirective;
 
+  constructor(@Inject(SWIPER_CONFIG) public config: SwiperConfigInterface) {
+  }
 
-  constructor() { }
-
-  ngOnInit() { }
+  ngOnInit() {
+  }
 
 }
